f04721abe49cbe614157cf610706260ff091fa8d
JJY Local Laptop <znrdlsxjspt1@gmail.com>
2021-08-01 22:37:33 +0900

    fix: execute-deploy.sh

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-nohup java -jar -Dspring.profiles.active=set0 target/Blog_-0.3.war &
+cd ~/Blog_Project/Travis/build/target
+nohup java -jar -Dspring.profiles.active=set0 Blog_-0.3.war >/dev/null 2>/dev/null <dev/null  &
